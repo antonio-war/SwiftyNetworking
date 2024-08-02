0f8fb1f4286b8b773f5fa3bc3d9ad1aa85547e1f
Antonio War <antonioguerra.it@gmail.com>
2024-08-02 11:53:24 +0200

    fix: remove unwanted unknown networking source

--- Sources/SwiftyNetworking/Models/SwiftyNetworkingSource.swift
+++ Sources/SwiftyNetworking/Models/SwiftyNetworkingSource.swift
@@ -10,16 +10,13 @@ import Foundation
 public enum SwiftyNetworkingSource: String, Equatable, Hashable, Sendable {
     case network
     case cache
-    case unknown
     
     init(resourceFetchType: URLSessionTaskMetrics.ResourceFetchType) {
         switch resourceFetchType {
-        case .networkLoad:
-            self = .network
         case .localCache:
             self = .cache
         default:
-            self = .unknown
+            self = .network
         }
     }
 }
