530a320872e3780a020628dd97dffc4c52f0cb84
Antonio War <antonioguerra.it@gmail.com>
2024-08-02 12:33:38 +0200

    feat: add SwiftyNetworkingCachePolicyTests

--- Tests/SwiftyNetworkingTests/Models/SwiftyNetworkingCachePolicyTests.swift
+++ Tests/SwiftyNetworkingTests/Models/SwiftyNetworkingCachePolicyTests.swift
@@ -5,31 +5,23 @@
 //  Created by Antonio Guerra on 02/08/24.
 //
 
+@testable import SwiftyNetworking
 import XCTest
 
 final class SwiftyNetworkingCachePolicyTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testReloadIgnoringCacheData() {
+        let cachePolicy = SwiftyNetworkingCachePolicy.reloadIgnoringCacheData
+        XCTAssertEqual(cachePolicy.underlyingCachePolicy, .reloadIgnoringLocalCacheData)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testReturnCacheDataElseLoad() {
+        let cachePolicy = SwiftyNetworkingCachePolicy.returnCacheDataElseLoad
+        XCTAssertEqual(cachePolicy.underlyingCachePolicy, .returnCacheDataElseLoad)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testReturnCacheDataDontLoad() {
+        let cachePolicy = SwiftyNetworkingCachePolicy.returnCacheDataDontLoad
+        XCTAssertEqual(cachePolicy.underlyingCachePolicy, .returnCacheDataDontLoad)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
