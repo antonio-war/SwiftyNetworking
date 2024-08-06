a821c5c917f0dd742c225a3f9f9abacb37286d6f
Antonio War <antonioguerra.it@gmail.com>
2024-08-06 11:25:15 +0200

    feat: add decoding for request methods

--- Sources/SwiftyNetworking/Clients/SwiftyNetworkingClient.swift
+++ Sources/SwiftyNetworking/Clients/SwiftyNetworkingClient.swift
@@ -15,10 +15,7 @@ public struct SwiftyNetworkingClient {
         configuration: URLSessionConfiguration = URLSessionConfiguration.default,
         delegate: SwiftyNetworkingDelegate = SwiftyNetworkingDelegate()
     ) {
-        self.session = URLSession(
-            configuration: configuration,
-            delegate: delegate
-        )
+        self.session = URLSession(configuration: configuration, delegate: delegate)
         self.delegate = delegate
     }
     
@@ -56,6 +53,39 @@ public struct SwiftyNetworkingClient {
     public func send(_ request: SwiftyNetworkingRequest) async throws -> SwiftyNetworkingResponse {
         try await withCheckedThrowingContinuation { continuation in
             send(request) { result in
+                switch result {
+                case .success(let response):
+                    continuation.resume(returning: response)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+    public func send<Model: Decodable>(_ request: SwiftyNetworkingRequest, decoding model: Model.Type, using decoder: JSONDecoder = JSONDecoder(), completion: @escaping (Result<Model, Error>) -> Void) {
+        send(request) { result in
+            do {
+                switch result {
+                case .success(let response):
+                    guard response.status == .success else {
+                        completion(.failure(URLError(.badServerResponse)))
+                        return
+                    }
+                    let model = try decoder.decode(model, from: response.body)
+                    completion(.success(model))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public func send<Model: Decodable>(_ request: SwiftyNetworkingRequest, decoding model: Model.Type, using decoder: JSONDecoder = JSONDecoder()) async throws -> Model {
+        try await withCheckedThrowingContinuation { continuation in
+            send(request, decoding: model, using: decoder) { result in
                 switch result {
                 case .success(let response):
                     continuation.resume(returning: response)
