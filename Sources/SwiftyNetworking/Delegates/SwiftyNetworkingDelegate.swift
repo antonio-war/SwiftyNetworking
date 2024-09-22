e4f3c6117e343a749bcb7731ddf6efa0db92ca1d
Antonio War <antonioguerra.it@gmail.com>
2024-09-22 16:16:51 +0200

    fix: strict concurrency issues in SwiftyNetworkingDelegate

--- Sources/SwiftyNetworking/Delegates/SwiftyNetworkingDelegate.swift
+++ Sources/SwiftyNetworking/Delegates/SwiftyNetworkingDelegate.swift
@@ -7,37 +7,50 @@
 
 import Foundation
 
-public class SwiftyNetworkingDelegate: NSObject, URLSessionTaskDelegate {
-    private var cache: NSCache<NSNumber, URLSessionTaskTransactionMetrics>
-    
+public class SwiftyNetworkingDelegate: NSObject, URLSessionTaskDelegate, @unchecked Sendable {
+    private let cache: NSCache<NSNumber, URLSessionTaskTransactionMetrics>
+    private let queue = DispatchQueue(label: "it.antoniowar.SwiftyNetworking.cache", attributes: .concurrent)
+
     public init(cache: NSCache<NSNumber, URLSessionTaskTransactionMetrics> = NSCache(countLimit: 5)) {
         self.cache = cache
     }
-        
+
     public func fetchType(for request: URLRequest) -> FetchType? {
-        guard let metrics = cache.object(forKey: request.hashValue as NSNumber) else {
-            return nil
+        return queue.sync {
+            guard let metrics = cache.object(forKey: request.hashValue as NSNumber) else {
+                return nil
+            }
+            return metrics.resourceFetchType
         }
-        return metrics.resourceFetchType
     }
-        
+
     public func start(for request: URLRequest) -> Date? {
-        guard let metrics = cache.object(forKey: request.hashValue as NSNumber) else {
-            return nil
+        return queue.sync {
+            guard let metrics = cache.object(forKey: request.hashValue as NSNumber) else {
+                return nil
+            }
+            return metrics.requestStartDate
         }
-        return metrics.requestStartDate
     }
-    
+
     public func end(for request: URLRequest) -> Date? {
-        guard let metrics = cache.object(forKey: request.hashValue as NSNumber) else {
-            return nil
+        return queue.sync {
+            guard let metrics = cache.object(forKey: request.hashValue as NSNumber) else {
+                return nil
+            }
+            return metrics.responseEndDate
         }
-        return metrics.responseEndDate
     }
-    
-    public func urlSession(_ session: URLSession, task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics) {
-        for metrics in metrics.transactionMetrics {
-            cache.setObject(metrics, forKey: metrics.request.hashValue as NSNumber)
+
+    public func urlSession(
+        _ session: URLSession,
+        task: URLSessionTask,
+        didFinishCollecting metrics: URLSessionTaskMetrics
+    ) {
+        queue.async(flags: .barrier) {
+            for metric in metrics.transactionMetrics {
+                self.cache.setObject(metric, forKey: metric.request.hashValue as NSNumber)
+            }
         }
     }
 }
