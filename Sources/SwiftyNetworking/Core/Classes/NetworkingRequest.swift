42059f460dc9c7cc48eef00f59de5c39978dc289
Antonio War <antonioguerra.it@gmail.com>
2025-06-13 18:40:38 +0200

    feat: clean request init from raw value

--- Sources/SwiftyNetworking/Core/Classes/NetworkingRequest.swift
+++ Sources/SwiftyNetworking/Core/Classes/NetworkingRequest.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
-public final class NetworkingRequest: Sendable, RawRepresentable {
+public struct NetworkingRequest: Sendable, RawRepresentable {
     public let url: URL
     public let method: NetworkingMethod
-    public let headers: [String: String]
-    public let body: Data?
+    public var headers: [String: String]
+    public var queryParameters: [String: String]
+    public var body: Data?
     public let cachePolicy: NetworkingCachePolicy
     public let timeout: TimeInterval
     
@@ -19,6 +20,7 @@ public final class NetworkingRequest: Sendable, RawRepresentable {
         url: URL,
         method: NetworkingMethod = .get,
         headers: [String : String] = [:],
+        queryParameters: [String: String] = [:],
         body: Data? = nil,
         cachePolicy: NetworkingCachePolicy = .returnCacheDataElseLoad,
         timeout: TimeInterval = 60
@@ -26,22 +28,23 @@ public final class NetworkingRequest: Sendable, RawRepresentable {
         self.url = url
         self.method = method
         self.headers = headers
+        self.queryParameters = queryParameters
         self.body = body
         self.cachePolicy = cachePolicy
         self.timeout = timeout
     }
         
-    public required init?(rawValue: URLRequest) {
-        guard let url = rawValue.url else { return nil }
-        guard let rawMethod = rawValue.httpMethod, let method = NetworkingMethod(rawValue: rawMethod) else { return nil }
-        guard let headers = rawValue.allHTTPHeaderFields else { return nil }
-        guard let cachePolicy = NetworkingCachePolicy(rawValue: rawValue.cachePolicy) else { return nil }
+    public init?(rawValue: URLRequest) {
+        guard let url = NetworkingRequest.url(rawValue: rawValue) else { return nil }
+        guard let method = NetworkingRequest.method(rawValue: rawValue) else { return nil }
+        guard let cachePolicy = NetworkingRequest.cachePolicy(rawValue: rawValue) else { return nil }
         self.url = url
         self.method = method
-        self.headers = headers
-        self.body = rawValue.httpBody
+        self.headers = NetworkingRequest.headers(rawValue: rawValue)
+        self.queryParameters = NetworkingRequest.queryParameters(rawValue: rawValue)
+        self.body = NetworkingRequest.body(rawValue: rawValue)
         self.cachePolicy = cachePolicy
-        self.timeout = rawValue.timeoutInterval
+        self.timeout = NetworkingRequest.timeout(rawValue: rawValue)
     }
     
     public var rawValue: URLRequest {
@@ -52,5 +55,49 @@ public final class NetworkingRequest: Sendable, RawRepresentable {
             request.allHTTPHeaderFields = headers
             return request
         }
+    }
+    
+    static func url(rawValue: URLRequest) -> URL? {
+        guard let url = rawValue.url else { return nil }
+        guard var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return url }
+        components.queryItems = nil
+        guard let url = components.url else { return url }
+        return url
+    }
+    
+    static func method(rawValue: URLRequest) -> NetworkingMethod? {
+        guard let rawMethod = rawValue.httpMethod else { return nil }
+        guard let method = NetworkingMethod(rawValue: rawMethod) else { return nil }
+        return method
+    }
+    
+    static func headers(rawValue: URLRequest) -> [String: String] {
+        guard let headers = rawValue.allHTTPHeaderFields else { return [:] }
+        return headers
+    }
+    
+    static func queryParameters(rawValue: URLRequest) -> [String: String] {
+        guard let url = rawValue.url else { return [:] }
+        guard let components = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return [:] }
+        guard let queryParameters = components.queryItems else { return [:] }
+        return Dictionary(uniqueKeysWithValues:
+            queryParameters.compactMap { item in
+                guard let value = item.value else { return nil }
+                return (item.name, value)
+            }
+        )
+    }
+    
+    static func body(rawValue: URLRequest) -> Data? {
+        return rawValue.httpBody
+    }
+    
+    static func cachePolicy(rawValue: URLRequest) -> NetworkingCachePolicy? {
+        guard let cachePolicy = NetworkingCachePolicy(rawValue: rawValue.cachePolicy) else { return nil }
+        return cachePolicy
+    }
+    
+    static func timeout(rawValue: URLRequest) -> TimeInterval {
+        return rawValue.timeoutInterval
     }
 }
